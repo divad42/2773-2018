2ada4f088182ce18057ad208bc6e6c9e45e2af97
divad42 <divadis42@gmail.com>
2018-01-20 15:36:33 -0600

    Initial commit

---
+++ src/org/usfirst/frc/team2773/robot/Robot.java
@@ -0,0 +1,155 @@
+// Version 0.0.0
+// Added the base code, plus rudamentary driving and grabbing code.
+
+package org.usfirst.frc.team2773.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.Joystick;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+   
+   public Victor FL;
+   public Victor FR;
+   public Victor BL;
+   public Victor BR;
+   
+   public Victor grabL;
+   public Victor grabR;
+   
+   public MecanumDrive drive;
+   public Joystick gamepad;
+   public Joystick stick;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+      
+      // wheels
+      FL = new Victor(0);
+      FR = new Victor(1);
+      BL = new Victor(2);
+      BR = new Victor(3);
+      
+      drive = new MecanumDrive(FL, BL, FR, BR);
+      
+      // grabber
+      grabL = new Victor(4);
+      grabR = new Victor(5);
+      
+      gamepad = new Joystick(0);
+      stick = new Joystick(1);
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// m_autoSelected = SmartDashboard.getString("Auto Selector",
+		// 		kDefaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+      drive.driveCartesian(gamepad.getRawAxis(1), gamepad.getRawAxis(0), gamepad.getRawAxis(2));
+      grabber();
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+   
+   public void setGrabbers(double val) {
+      grabL.set(val);
+      grabR.set(val);
+   }
+   
+   public void grabber() {
+      if(stick.getRawButton(1))  // trigger used to eject the cube
+         setGrabbers(0.5);
+      else if(stick.getRawButton(2))   // side button used to take in the cube
+         setGrabbers(-0.5);
+   }
+}
+
+                                                  /*:-                          
+                                                 /hdms                          
+                                                `ommmh.                          
+                                               `+mmmd/os+-`                      
+                                             `:ohmmmmdmmmmmhs/                    
+                                    -+oooo/+sdmmmmbmanmmmmmmmd``                  
+             ```...--..```        `+dmyyyhmmmmmmmmmneedsmmmmmmhyy+                
+      ``-/+syhhdmmmmmmmdhyso+++++sydms    hmmmmdmmmmtommmmmmmmmmmd`               
+     /ydmmmmmmmmmmmmmmmmmmmmmmy:./oo/`    .+so/./hmmmstopmmmmmmmmm+               
+      -odmmmmmmmjaredmmmmmmmmh`                  .dmmmmmmmmmmmmmmdh               
+     .-`.+hmmmmmmismmmmmmmmmmms                  hmmmmmmmmmdhddy+-`               
+    `odmmdhshmmmmmthemmmmmmmmmd/     `+shys/      odmmmmdy+-` ``                   
+   `ymmmmmkhaimmmmmrealmmmmm:.      `hhailmmy      `-omm-                          
+   smmmmmmmismmmmmmmmvpmmmmm        :mmthemmm`       -mm:                          
+  `mmmmmmmmmbadmmmmmmmmmmmmmyo/`     ommorbd/     `/oymd.                          
+  -mmmsquidmmmmmmmmmmmmyommmmmms      `:+/:`     `dmds+.                           
+  `dmmmtastesmmmmmmmd/`hmmmmmd:                  smm/                             
+   ommmmmmgoodmmmmmdo` `oydmmmh-   ``        ``   /mmo                             
+   :malecmmmmmmmmdo.     ``-+syhsohddh-    +hddy+hmd+`                             
+   :mmisn'tmmmmd+.            ``.-:+hmy-.-:md+oyyy+`                               
+   ommmrealmmh+`                    /mmddddm/  ``                                  
+   `dmmmmmmmy/`                      -josephd                                       
+   +mmmmmds-`                         ydunlap                                       
+  .dmmmh+.                            .herwin`                                      
+ `ymmd/`                               .hmmmmo.                                     
+ommmmh/`                               .smmmmdy/.                                  
+`:::::::.   Mr. Meckling is Max's dad.  `:::::::*/
