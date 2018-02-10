01d2b1ea182cb75839fdef71524aeba6cd5f99f9
Yeahwon Lee <yeahwonlee@users.noreply.github.com>
2018-02-10 14:15:46 -0600

    added encoder rates

--- src/org/usfirst/frc/team2773/robot/Robot.java
+++ src/org/usfirst/frc/team2773/robot/Robot.java
@@ -32,6 +32,11 @@ public class Robot extends TimedRobot {
    public Victor FR;
    public Victor BL;
    public Victor BR;
+   
+   static public Encoder FLE;
+   static public Encoder FRE;
+   static public Encoder BLE;
+   static public Encoder BRE;
 
    public Spark lowerBar;
    public Spark upperBar;
@@ -84,6 +89,11 @@ public class Robot extends TimedRobot {
       BR = new Victor(2);
       
       drive = new MecanumDrive(FL, BL, FR, BR);
+      //PORT NUMS TEMPORARY!!!
+      FLE = new Encoder(1,2);
+      FRE = new Encoder(2,3);
+      BLE = new Encoder(3,4);
+      BRE = new Encoder(4,5);
       
       // grabber
       grab = new Spark(4);
@@ -290,9 +300,20 @@ public class Robot extends TimedRobot {
    public void fullBar(double val) {
 	   
    }
-   
+   public static void displayEncoderRates(){
+      double[] rates = new double[4];
+      rates[0] = this.FRE.getRate();
+      rates[1] = this.FLE.getRate(); 
+      rates[2] = this.BRE.getRate(); 
+      rates[3] = this.BLE.getRate(); 
+      
+      for(int i = 0; i ++; i < rates.length){
+         SmartDashBoard.putNumber("Rate of Encoder " + i, rates[i]);
+      }
+ 
+   }
    public void output() {
-   
+      displayEncoderRates();
 	   DriverStation ds= DriverStation.getInstance();
       // display the values from the encoder to the SmartDashboard
 	   SmartDashboard.putString("GameData", ds.getGameSpecificMessage());
